226815cff89a15d9dd6e68f7e4e7d10c8609806b
stevenfleming <12flemings@gmail.com>
2020-05-08 14:42:41 -0700

    At this point, after through research it appears there is no publicly avaliable database that stores info regarding where a movie is currently hosted

--- src/components/ApiCall.js
+++ src/components/ApiCall.js
@@ -39,20 +39,3 @@ class ApiCall extends React.Component {
 
 export default ApiCall;
 
-// makeApiCall() {
-//   fetch(`url.${process.env.REACT_APP_API_KEY}`)
-//     .then(response => response.json())
-//     .then(
-//       (jsonifiedResponse) => {
-//         this.setState({
-//           isLoaded: true,
-//           headlines: jsonifiedResponse.results
-//         });
-//       })
-//     .catch((error) => {
-//       this.setState({
-//         isLoaded: true,
-//         error
-//       });
-//     });
-// }
